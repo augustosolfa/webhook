65a5efeff3b831c66cf6ad36a806a95975dd7e29
Augusto Solfa <augusto@solfa.net>
2022-11-15 17:36:24 -0300

    Add client

--- client/index.js
+++ client/index.js
@@ -1,8 +1,30 @@
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 render();
 
 async function render() {
-  const calls = await fetch('http://localhost:3000/');
-  console.log(calls);
+  const calls = await fetch("http://localhost:3000/").then((response) =>
+    response.json()
+  );
+  calls.sort((a, b) => b.time - a.time);
+  for (let index in calls) {
+    const call = calls[index];
+    const row = document.createElement("div");
+    row.classList = 'row';
+    
+    const pTime = document.createElement('p');
+    pTime.classList = 'time';
+    pTime.innerText = new Date(call.time - (60*60*3*1000)).toISOString();
+    row.appendChild(pTime);
+    const pPath = document.createElement('p');
+    pPath.classList = 'path';
+    pPath.innerText = call.path;
+    row.appendChild(pPath);
+    const pBody = document.createElement('p');
+    pBody.classList = 'body';
+    pBody.innerText = JSON.stringify(call.body, null, 1);
+    row.appendChild(pBody);
+
+    root.appendChild(row);
+  }
 }
